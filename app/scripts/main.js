03c09c8277ddb2084a2bb4d87460ef4c0b66d97e
Jim <jim_yuan@brainet.com.cn>
2013-12-05 18:49:03 +0800

    g01:指定按键速度快慢的判断算法，demo.html实现纯css3 animation动画

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,28 @@
-console.log('\'Allo \'Allo!');
++function ($) { "use strict";
+  var J = {
+    timeStamp : 0,
+    _m : {
+      speed:function(n){// speed level 
+        var level=[0,100,110,120,130,140,150,300,500,700,800];
+        return level.concat(n).sort(function(x, y){return y - x;}).indexOf(n);
+      }
+    }
+  };
+
+  J.move=function(){
+    $(document).on("keypress", function(e){e.preventDefault();});
+    $("i.fa-play-circle-o").on("click" , function(e){
+      J.timeStamp=e.timeStamp;
+      $(document).on("keyup", function(e){
+        if(e.which===32){
+          var step = e.timeStamp - J.timeStamp;
+          var speed= J._m.speed(step);
+          $(".main-scene").css("background-position", "+="+speed*20);
+          console.log(step);
+          J.timeStamp = e.timeStamp;
+        }
+      });
+      $(this).fadeOut();
+    });
+   }();
+}(jQuery);
