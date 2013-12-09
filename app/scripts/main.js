daa9c650c2c52a623959cfc7b3c72136c0475eb4
jun <jun_12@163.com>
2013-12-09 18:37:16 +0800

    g01:场景整合

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,11 @@
 +function ($) { "use strict";
   var J = {
     timeStamp : 0,
+    count:0,
+    currTime:60,
     _m : {
       speed:function(n){// speed level 
-        var level=[200,400,600];
+        var level=[100,150,600,2000];
         return level.concat(n).sort(function(x, y){return y - x;}).indexOf(n);
       }
     }
@@ -12,18 +14,27 @@
   J.move=function(){
     $(document).on("keypress", function(e){e.preventDefault();});
     $("i.fa-play-circle-o").on("click" , function(e){
+    var timeInt = window.setInterval(function(){
+    autotime();
+    if(J.currTime == 0){
+      //game over
+    } 
+    $("#timeCount span").text(J.currTime);},1000);
       J.timeStamp=e.timeStamp;
-      var speedStyle=["WalkSlow","WalkFast","RunSlow","RunFast"];
+      var speedStyle=["WalkStop","WalkSlow","WalkFast","RunSlow","RunFast"];
       $(document).on("keyup", function(e){
         if(e.which===32){
+          J.count++;
+          //点击次数
+          $("#count span").text(J.count);
           var step = e.timeStamp - J.timeStamp;
           var speed= J._m.speed(step);
-          $(".main-scene").css("background-position", "+="+speed*20)
+          $(".main-scene").css("background-position", "+="+speed*250)
           .children(":first-child").attr("id","warphorse"+speedStyle[speed]);
           $(".main-scene").on("webkitTransitionEnd", function(){
             $(this).children(":first-child").attr("id","warphorse"+speedStyle[0]);
           });
-          console.log(step);
+          console.log(J.count);
           J.timeStamp = e.timeStamp;
         }
       });
@@ -31,8 +42,16 @@
     });
    }();
 
+   /**
+   倒计时
+   */
+  function autotime(){
+    J.currTime --;
+    $("#timeCount span").text(J.currTime); 
+  }
+
+
    /*
     切换头像
    */
-   
 }(jQuery);
